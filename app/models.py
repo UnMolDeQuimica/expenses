03e960eb061c7d8f889237cbe50aa394ec68f578
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-07-20 21:32:41 +0200

    WIP

--- app/models.py
+++ app/models.py
@@ -1,7 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-# Create your models here.
-#
-class Test(models.Model):
+class BaseModel(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+
+
+class MovementType(BaseModel):
     name = models.CharField()
+    description = models.TextField()
+
+
+class Movement(BaseModel):
+    BALANCES = (("input", "Input"), ("expense", "Expense"))
+    name = models.CharField()
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    movement_type = models.ForeignKey(to=MovementType, on_delete=models.CASCADE)
+    balance = models.CharField(choices=BALANCES)
+    date = models.DateTimeField(auto_now_add=True)
